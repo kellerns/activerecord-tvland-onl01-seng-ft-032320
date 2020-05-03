981e5c4fa1090ab0656b39c5859306d4339ae903
Nicholas Keller <nic.keller328@gmail.com>
2020-05-03 21:19:12 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -3,7 +3,6 @@ class CreateActors < ActiveRecord::Migration[5.1]
     create_table :actors do |t|
       t.string :first_name
       t.string :last_name
-      t.string :catchphrase
     end
   end
 end
