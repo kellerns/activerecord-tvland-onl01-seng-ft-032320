a3aeb45b31d93bd821c80d3cc5698e090cbcf3de
Nicholas Keller <nic.keller328@gmail.com>
2020-05-03 21:14:27 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,11 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+
+  def actors_list
+    self.characters.collect do |char|
+      "#{char.actor.first_name} #{char.actor.last_name}""
+  end
+
+
 end
