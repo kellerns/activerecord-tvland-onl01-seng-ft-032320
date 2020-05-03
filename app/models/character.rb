cd55ed2c411faaf2f9eda020409c1735ba65bab9
Nicholas Keller <nic.keller328@gmail.com>
2020-05-03 21:12:06 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,10 @@
 class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.first_name} #{self.last_name} always says: #{self.catchphrase}"
+  end
+
+
 end
